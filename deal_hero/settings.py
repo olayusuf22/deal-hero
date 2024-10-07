c4bc7d42cc385ceb2c15b77ff8f44bfa37866b00
OYusuf <olayusuf822@gmail.com>
2024-10-07 14:51:23 -0500

    db username and pd create for settings py

--- deal_hero/settings.py
+++ deal_hero/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'deal_hero.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'deal-hero',
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PW'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': '5432',
     }
 }
 
