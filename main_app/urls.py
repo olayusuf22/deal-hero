0311069c2bdf5bce1675f3e0b854833bd87e1bfa
ahmed2704 <kassimahmed2704@gmail.com>
2024-10-11 11:36:28 -0400

    add to wishlist buttons

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
     path('search/', views.product_search, name='product_search'),
+    path('product/<str:product_id>/fetch/', views.fetch_product_details, name='fetch-product-details'),
 ]
