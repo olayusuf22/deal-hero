d66452797bf7a4fdecd422904a1cefe28031191b
JP_Escobari <jpe911@gmail.com>
2024-10-17 10:06:40 -0400

    Hot fix for password validation (Oops)

--- deal_hero/settings.py
+++ deal_hero/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
 }
 
 
-Password validation
-https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
